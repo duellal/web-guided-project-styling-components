a5b005b843b88f929c29afeb585f1a9d3c127596
Casey Harding <casey.r.harding@gmail.com>
2022-06-16 12:34:19 -0400

    Initial Friend.js styling in place.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,9 @@
 // theme object here
+export default {
+  primaryColor: "orange",
+  secondaryColor: "copper",
+  tertiaryColor: "lemonchiffon",
+  black: "black",
+  white: "white",
+  breakpointMobile: "(max-width: 550px)"
+}
